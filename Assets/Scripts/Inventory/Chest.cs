32f338aa332b93e72fe8a93a3306feef62847258
ThomasVersaevel <thomas0699@gmail.com>
2023-01-14 23:14:09 +0100

    Chest
    - emission not proper
    - animation works on trigger enter

--- Assets/Scripts/Inventory/Chest.cs
+++ Assets/Scripts/Inventory/Chest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimatedTile : MonoBehaviour
+public class Chest : MonoBehaviour
 {
     Animator anim;
     // Start is called before the first frame update
@@ -19,9 +19,5 @@ public class AnimatedTile : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         anim.SetTrigger("Collision");
-    }
-    private void OnTriggerExit2D(Collider2D collision) 
-    {
-        anim.ResetTrigger("Collision");
     }
 }
